a4b52e3a4c2f9dbc1a00aa4595c32be85826d670
y-sugawara0508 <sugawara.yuto@marble-corp.co.jp>
2025-05-01 09:32:52 +0900

    Sample01.javaの更新

--- src/lesson01/Sample01.java
+++ src/lesson01/Sample01.java
@@ -1,9 +1,15 @@
 package lesson01;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Sample01 {
 	public static void main(String[] args) {
 		
 		// 標準出力
 		System.out.println("Hello world!!");
+		
+		// 入力処理
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
